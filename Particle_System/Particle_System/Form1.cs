2fc45d24e90f895a0047e9b5b8e38c7828bd09d7
Flarishon <sizyhanna678@gmail.com>
2026-05-27 11:20:18 +0800

    Пройден этап 'Система частиц, часть 1'

---
+++ Particle_System/Particle_System/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Particle_System
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();
+        Emitter emitter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // а тут теперь вручную создаем
+            emitter = new TopEmitter
+            {
+                Width = picDisplay.Width,
+                GravitationY = 0.25f
+            };
+
+            /* пока отключим точки
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.25),
+                Y = picDisplay.Height / 2
+            });
+
+            emitter.impactPoints.Add(new AntiGravityPoint
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            });
+
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.75),
+                Y = picDisplay.Height / 2
+            });
+            */
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState();
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g);
+            }
+
+            picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
+        }
+    }
+}
